cf6f7ffe20fe777c897b6752147dbd7be25b3347
Mansi Patle <mansipatle0@gmail.com>
2025-11-11 17:10:21 +0530

    Update backend CORS,frontend fetch to use env variables

--- .history/server_20251111163755.js
+++ .history/server_20251111163755.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://crm-telecalling-app.onrender.com"],
+    origin: ["http://localhost:3000","https://your-frontend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
